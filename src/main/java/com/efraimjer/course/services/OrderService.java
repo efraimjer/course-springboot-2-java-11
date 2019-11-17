26d221984fb0131e0e88978a0813e66a7dda79b6
Efraim Jerszurki <efraimjerszurki@gmail.com>
2019-11-17 00:07:20 -0300

    Order, ISO8601 PART 02

---
+++ src/main/java/com/efraimjer/course/services/OrderService.java
@@ -0,0 +1,28 @@
+package com.efraimjer.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.efraimjer.course.entities.Order;
+import com.efraimjer.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional <Order> obj = repository.findById(id);
+		
+		return obj.get();
+	}
+
+}
