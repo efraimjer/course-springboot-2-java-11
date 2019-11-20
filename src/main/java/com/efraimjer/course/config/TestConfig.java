b8617844f768a5d0e9fa741848a90886fa04fac1
Efraim Jerszurki <efraimjerszurki@gmail.com>
2019-11-19 22:52:08 -0300

    Payment, one-to-one association

--- src/main/java/com/efraimjer/course/config/TestConfig.java
+++ src/main/java/com/efraimjer/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.efraimjer.course.entities.Category;
 import com.efraimjer.course.entities.Order;
 import com.efraimjer.course.entities.OrderItem;
+import com.efraimjer.course.entities.Payment;
 import com.efraimjer.course.entities.Product;
 import com.efraimjer.course.entities.User;
 import com.efraimjer.course.entities.enums.OrderStatus;
@@ -81,8 +82,12 @@ public class TestConfig implements CommandLineRunner {
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
-
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
 		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+
 
 		
 	}
